ade6f5a9a3bf50939e011c970e126c6a04be7443
Rahul Mangal <rahul200826101998@gmail.com>
2021-04-21 17:22:17 +0530

    Add bookmark form

--- markers/urls.py
+++ markers/urls.py
@@ -6,4 +6,5 @@ app_name = 'markers'
 urlpatterns = [
     path('bookmarks/', views.BookmarkListView.as_view(), name='bookmark-list'),
     path('bookmark/<int:pk>/', views.BookmarkDetailView.as_view(), name='bookmark-detail'),
+    path('add_bookmark/', views.add_bookmark, name='add-bookmark'),
 ]
--- markers/views.py
+++ markers/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
-from .models import Bookmark
-from .filters import BookmarkFilter
+from markers.models import Bookmark
+from markers.filters import BookmarkFilter
+from markers.forms import BookmarkForm 
+from django.db import transaction
 
 class BookmarkListView(ListView):
     model = Bookmark
@@ -14,3 +16,15 @@ class BookmarkListView(ListView):
 class BookmarkDetailView(DetailView):
     model = Bookmark
 
+@transaction.atomic
+def add_bookmark(request):
+    bookmark_form = BookmarkForm(request.POST or None)
+
+    if bookmark_form.is_valid():
+        bookmark = bookmark_form.save()
+        return redirect('markers:bookmark-list')
+
+    context = {
+        "bookmark_form": bookmark_form,
+    }
+    return render(request, 'markers/add_bookmark.html', context)
