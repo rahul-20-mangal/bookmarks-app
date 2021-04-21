814ea4c4f9dd5718d5737f3ce7dadf58bafb7cf2
Rahul Mangal <rahul200826101998@gmail.com>
2021-04-21 18:50:13 +0530

    Add functionalities of Edit and delete bookmarks

--- markers/urls.py
+++ markers/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('bookmarks/', views.BookmarkListView.as_view(), name='bookmark-list'),
     path('bookmark/<int:pk>/', views.BookmarkDetailView.as_view(), name='bookmark-detail'),
     path('add_bookmark/', views.add_bookmark, name='add-bookmark'),
+    path('edit_bookmark/<int:pk>/', views.edit_bookmark, name='edit-bookmark'),
+    path('delete_bookmark/<int:pk>/', views.delete_bookmark, name='delete-bookmark'),
 ]
--- markers/views.py
+++ markers/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView
 from markers.models import Bookmark
 from markers.filters import BookmarkFilter
 from markers.forms import BookmarkForm 
 from django.db import transaction
+
 
 class BookmarkListView(ListView):
     model = Bookmark
@@ -13,8 +14,10 @@ class BookmarkListView(ListView):
         context['filter'] = BookmarkFilter(self.request.GET, queryset=self.get_queryset())
         return context
 
+
 class BookmarkDetailView(DetailView):
     model = Bookmark
+
 
 @transaction.atomic
 def add_bookmark(request):
@@ -28,3 +31,27 @@ def add_bookmark(request):
         "bookmark_form": bookmark_form,
     }
     return render(request, 'markers/add_bookmark.html', context)
+
+
+@transaction.atomic
+def edit_bookmark(request, pk):
+    bookmark = get_object_or_404(Bookmark, pk=pk)
+
+    bookmark_form = BookmarkForm(request.POST or None, instance=bookmark)
+
+    if bookmark_form.is_valid():
+        bookmark_form.save()
+        return redirect('markers:bookmark-detail', pk=pk)
+    
+    context = {
+        "bookmark_form": bookmark_form
+    }
+    return render(request, 'markers/add_bookmark.html', context)
+
+
+@transaction.atomic
+def delete_bookmark(request, pk):
+    bookmark = get_object_or_404(Bookmark, pk=pk)
+    bookmark.delete()
+
+    return redirect('markers:bookmark-list')
