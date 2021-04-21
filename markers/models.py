35cb47091f8833f569a6250d746168ad7c051916
Rahul Mangal <rahul200826101998@gmail.com>
2021-04-21 11:04:21 +0530

    Create Bookmark and Folder model

--- markers/models.py
+++ markers/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 # Create your models here.
+class Bookmark(models.Model):
+    name = models.CharField(max_length=512)
+    url = models.URLField(null=True, blank=True)
+    description = models.TextField()
+    tags = TaggableManager()
+    folder = models.ForeignKey('Folder', on_delete=models.CASCADE)
+
+class Folder(models.Model):
+    name = models.CharField(max_length=200)
